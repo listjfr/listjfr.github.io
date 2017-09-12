c2b893f7c920fcb08f2e79c03d259e79e743de96
listjfr <listjfr@users.noreply.github.com>
2017-09-12 10:13:17 -0400

    Add jquery code from html

--- jquery.js
+++ jquery.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-                $("div").on({
-                    mouseenter: function () {
-                        $(this).fadeTo("fast", 0.5);
-                    },
-                    mouseleave: function () {
-                        $(this).fadeTo("fast", 1);
-                    },
-                    click: function () {
-                        $(this).fadeTo("slow", 0);
-                    }
-                    
-                });
-            });
+    $(".squares div").on({
+        mouseenter: function () {
+            $(this).fadeTo("slow", 0.2);
+        },
+        mouseleave: function () {
+            $(this).fadeTo("slow", 1);
+        },
+        click: function () {
+            $(this).fadeTo("fast", 0);
+        }
+    });
+
+    $("#pinkScheme").click(function () {
+        $(".squares").removeClass("blue");
+        $(".squares").addClass("pink");
+    });
+
+    $("#blueScheme").click(function () {
+        $(".squares").removeClass("pink");
+        $(".squares").addClass("blue");
+    });
+
+});
